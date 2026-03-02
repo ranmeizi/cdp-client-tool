4ca211cf22816133a0749316414c882d111d7011
boboan <360969885@qq.com>
2026-03-02 17:31:53 +0800

    fix: client example

--- examples/client/index.ts
+++ examples/client/index.ts
@@ -2,7 +2,7 @@
  * client 子工程引用根目录开发的 cdp-client-tool 模块
  * 使用前需在仓库根目录执行: pnpm run build
  */
-import { Client, EVENTS } from "@bomon/cdp-client-tool";
+import { Client, EVENTS } from "cdp-client-tool";
 
 const client = new Client({
   deviceName: "example-device",
